ccab5e68d5c6424890b55ac2f59c0c581349dec8
Andreas von Holy <andreas409@gmail.com>
2015-07-01 11:52:55 +0200

    basic player animation added to model and a animation controller configured

---
+++ Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+
+	private CharacterController controller;
+	public Vector3 speed;
+
+	// Use this for initialization
+	void Start () {
+		controller = gameObject.GetComponent<CharacterController>();
+		if (speed == null){
+			speed = new Vector3(10,4,10);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Input.GetAxis
+	}
+}
