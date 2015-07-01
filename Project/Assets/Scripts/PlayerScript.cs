c5926872376941f55153d4169fe3c3f05a2a9ee4
Andreas von Holy <andreas409@gmail.com>
2015-07-01 12:44:18 +0200

    player can move without animation

--- Project/Assets/Scripts/PlayerScript.cs
+++ Project/Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,7 @@ public class PlayerScript : MonoBehaviour {
 
 	private CharacterController controller;
 	public Vector3 speed;
+	public Vector3 move;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +14,35 @@ public class PlayerScript : MonoBehaviour {
 		if (speed == null){
 			speed = new Vector3(10,4,10);
 		}
+		move = new Vector3(0,0,0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Input.GetAxis
+		float inputX = Input.GetAxis("Horizontal");
+		float inputZ = Input.GetAxis("Vertical");
+		move = new Vector3(0,0,0);
+
+		if (inputX > 0){
+			move = new Vector3(speed.x,move.y,move.z);
+			print("pos x");
+		}
+		else if (inputX < 0){
+			move = new Vector3(-speed.x,move.y,move.z);
+			print("neg x");
+		}
+
+		if (inputZ > 0){
+			move = new Vector3(move.x,move.y,speed.z);
+			print("pos y");
+		}
+		else if (inputZ < 0){
+			move = new Vector3(move.x,move.y,-speed.z);
+			print("neg y");
+		}
+
+		controller.Move(move);
+
+
 	}
 }
