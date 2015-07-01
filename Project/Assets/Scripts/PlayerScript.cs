46c86ececebda8c9481131a295b250bd415919d2
Andreas von Holy <andreas409@gmail.com>
2015-07-01 14:35:19 +0200

    player movement fixed and mouse incoporated, also light placed at moon in relation to skybox and softned shadows a bit

--- Project/Assets/Scripts/PlayerScript.cs
+++ Project/Assets/Scripts/PlayerScript.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-
+	//the controller that we use to move the player
 	private CharacterController controller;
-	public Vector3 speed;
-	public Vector3 move;
+	//the speed of the character
+	public Vector3 charSpeed;
+	//the speed at which the mouse rotates
+	public Vector3 mouseSpeed;
+	//a vector we use to move the player
+	private Vector3 move;
 
-
+	//the animator to handle the animations for the player
 	public Animator animator;
+
+	//float value for the rotation on the vertical axis
+	private float rotationValue;
+
+	//float for the jumping velocity calculations
+	private float jumpingVelocity;
 
 	//tut
 	public float smoothing = 15f;
@@ -18,21 +28,79 @@ public class PlayerScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//we set up the movement speed to be equal on the x,z and y is the jumping speed
+		charSpeed = new Vector3(7f,5f,7f);
+		//we set the mouse speed
+		mouseSpeed = new Vector3(3f,3f,3f);
+
+		//this allows the mouse to go off the screen
+		Screen.lockCursor = true;
+
+		//we get the animator component
 		animator = gameObject.GetComponent<Animator>();
+		//we get the character controller component
 		controller = gameObject.GetComponent<CharacterController>();
-		if (speed == null){
-			speed = new Vector3(10,4,10);
-		}
+		//default
 		move = new Vector3(0,0,0);
+		rotationValue = 0f;
 	}
 
 
-	void FixedUpdate(){
-		float inputX = Input.GetAxis("Horizontal");
-		float inputZ = Input.GetAxis("Vertical");
 
 
-		move = new Vector3(inputX,0,inputZ);
+	void rotatePlayer(float mouseX, float mouseY){
+		//we rotate on the horzontal axis
+		transform.Rotate(0,mouseX,0);
+		
+		//we rotate on the vertical axis
+		rotationValue -= mouseY;
+		rotationValue = Mathf.Clamp(rotationValue, -60f, 60f);
+		Camera.main.transform.localRotation = Quaternion.Euler(rotationValue,0,0);
+	}
+
+	void movePlayer(float moveX, float moveZ){
+		//we check if the player is trying to jump while on the floor, check for button press first as this will allow less checks on for the grounded flag
+		if(Input.GetButtonDown("Jump") && controller.isGrounded){
+			jumpingVelocity = charSpeed.y;
+		}
+		//we calculate in the gravity for the jump
+		jumpingVelocity += Physics.gravity.y * Time.deltaTime;
+		
+		//we calcuate the amount we move by the speed
+		float speedX = moveX * charSpeed.x;
+		float speedZ = moveZ * charSpeed.z;
+		
+		//we make our new vextor with all 3d velocities added
+		move = new Vector3(speedX,jumpingVelocity,speedZ);
+		
+		//we factor in the rotation
+		move = transform.rotation * move;
+	}
+
+	void Update(){
+		//we get the key movement of the player on the hor and ver axis
+		float moveX = Input.GetAxis("Horizontal");
+		float moveZ = Input.GetAxis("Vertical");
+		//we get the mouse movement of the player
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+		//we calculate the rotation for the mouse movement
+		float rotationX = mouseX * mouseSpeed.x;
+		float rotationY = mouseY * mouseSpeed.y;
+		//send through the values to rotate
+		rotatePlayer(rotationX,rotationY);
+
+		//we send through the data to move the player
+		movePlayer(moveX,moveZ);
+
+
+		//we now send this information through to the character controller
+		controller.Move(move * Time.deltaTime);
+
+
+		
+		/*move = new Vector3(inputX,0,inputZ);
 
 		
 		if (inputX != 0 || inputZ != 0){
@@ -42,7 +110,7 @@ public class PlayerScript : MonoBehaviour {
 			Quaternion rotation = Quaternion.Lerp(rigidbody.rotation, direction, smoothing*Time.deltaTime);
 			rigidbody.MoveRotation(rotation);
 			controller.Move(move);
-		}
+		}*/
 
 
 
