3938586b5746371c4e913cd14456551c676beafe
Andreas von Holy <andreas409@gmail.com>
2015-07-02 18:57:57 +0200

    basic implementation of the rotating camera

---
+++ Project/Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitCamera : MonoBehaviour {
+
+
+	public float height;
+	public float radius;
+	public float rotationSpeed;
+	//public Vector3 target;
+	public Transform target;
+	private Camera camera;
+
+
+	private float angle;
+	private float hyp;
+
+	// Use this for initialization
+	void Start () {
+		camera = this.gameObject.GetComponent<Camera>();
+		height = 200;
+		radius = 100;
+		rotationSpeed = 30;
+		hyp = Mathf.Sqrt((height*height) + (radius * radius));
+		angle = Mathf.Asin(height/hyp);
+		print ("height " + height);
+		print ("hypotenuse " + hyp);
+		print ("radius " + radius);
+		print ("angle in r " + angle);
+		print ("angle in d " + angle*(180/Mathf.PI));
+		//transform.Rotate(angle,0,0);
+		//transform.Rotate(angle,0,0, Space.Self);
+		transform.Rotate(angle,0,0, Space.World);
+		//target = new Vector3(-149.0f, 0.63f, -2.56f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position = target.position + (transform.position - target.position).normalized * radius;
+		if (target != null){
+			transform.position = new Vector3(target.position.x + radius, target.position.y + height, target.position.z + radius);
+
+			transform.RotateAround(target.position, Vector3.up, rotationSpeed * Time.deltaTime);
+		}
+		else{
+			print ("Please assign target first!");
+		}
+	}
+}
