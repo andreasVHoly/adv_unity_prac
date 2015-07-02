ac92ed44e1b72218532ff686393d0c28761ead4e
Andreas von Holy <andreas409@gmail.com>
2015-07-02 19:16:16 +0200

    got the angle working for the camera, having diffuclty moving origin instead of view volume?

--- Project/Assets/Scripts/OrbitCamera.cs
+++ Project/Assets/Scripts/OrbitCamera.cs
@@ -22,15 +22,17 @@ public class OrbitCamera : MonoBehaviour {
 		radius = 100;
 		rotationSpeed = 30;
 		hyp = Mathf.Sqrt((height*height) + (radius * radius));
-		angle = Mathf.Asin(height/hyp);
+		angle = Mathf.Asin(radius/hyp);
 		print ("height " + height);
 		print ("hypotenuse " + hyp);
 		print ("radius " + radius);
 		print ("angle in r " + angle);
 		print ("angle in d " + angle*(180/Mathf.PI));
-		//transform.Rotate(angle,0,0);
+		print ("new angle " + (angle*(180/Mathf.PI)+90));
+
+		transform.Rotate(angle*(180/Mathf.PI),0,0);
 		//transform.Rotate(angle,0,0, Space.Self);
-		transform.Rotate(angle,0,0, Space.World);
+		//transform.Rotate(angle,0,0, Space.World);
 		//target = new Vector3(-149.0f, 0.63f, -2.56f);
 	}
 	
@@ -39,7 +41,8 @@ public class OrbitCamera : MonoBehaviour {
 		//transform.position = target.position + (transform.position - target.position).normalized * radius;
 		if (target != null){
 			transform.position = new Vector3(target.position.x + radius, target.position.y + height, target.position.z + radius);
-
+			//transform.rotation.x = angle;
+			//transform.Rotate(0,rotationSpeed * Time.deltaTime,0, Space.World);
 			transform.RotateAround(target.position, Vector3.up, rotationSpeed * Time.deltaTime);
 		}
 		else{
