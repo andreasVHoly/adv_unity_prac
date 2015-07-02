296802793225385bad4d0f14645b90b1e1e7630f
Andreas von Holy <andreas409@gmail.com>
2015-07-02 19:59:53 +0200

    camera orbits player properly, might need to adjust fasr clipping range properly

--- Project/Assets/Scripts/OrbitCamera.cs
+++ Project/Assets/Scripts/OrbitCamera.cs
@@ -20,7 +20,7 @@ public class OrbitCamera : MonoBehaviour {
 		camera = this.gameObject.GetComponent<Camera>();
 		height = 200;
 		radius = 100;
-		rotationSpeed = 30;
+		rotationSpeed = 0.01f;
 		hyp = Mathf.Sqrt((height*height) + (radius * radius));
 		angle = Mathf.Asin(radius/hyp);
 		print ("height " + height);
@@ -35,15 +35,44 @@ public class OrbitCamera : MonoBehaviour {
 		//transform.Rotate(angle,0,0, Space.World);
 		//target = new Vector3(-149.0f, 0.63f, -2.56f);
 	}
-	
+
+
+	public void rotateCamera(){
+		float sinShift = Mathf.Sin(rotationSpeed);
+		float cosShift = Mathf.Cos(rotationSpeed);
+
+		//we back up the position of the target
+		float targetX = target.transform.position.x;
+		float targetZ = target.transform.position.z;
+
+		//we calcualte the position of the camera if it where translated to the origin by subtracting the position of our target
+		float pX = this.transform.position.x - targetX;
+		float pZ = this.transform.position.z - targetZ;
+
+		//we calculate the new positions based in our cos/sin shifts
+		float posX = pX * cosShift - pZ * sinShift;
+		float posZ = pX * sinShift + pZ * cosShift;
+
+		//we assign the new position
+		transform.position = new Vector3(posX + targetX,transform.position.y, posZ + targetZ );
+	}
+
+	public void rotateView(){
+		Vector3 targetPos = new Vector3(target.transform.position.x,-1,target.transform.position.z);
+		transform.LookAt(targetPos);
+	}
+
+
 	// Update is called once per frame
 	void Update () {
 		//transform.position = target.position + (transform.position - target.position).normalized * radius;
 		if (target != null){
-			transform.position = new Vector3(target.position.x + radius, target.position.y + height, target.position.z + radius);
+			//transform.position = new Vector3(target.position.x + radius, target.position.y + height, target.position.z + radius);
 			//transform.rotation.x = angle;
 			//transform.Rotate(0,rotationSpeed * Time.deltaTime,0, Space.World);
-			transform.RotateAround(target.position, Vector3.up, rotationSpeed * Time.deltaTime);
+			//transform.RotateAround(target.position, Vector3.up, rotationSpeed * Time.deltaTime);
+			rotateCamera();
+			rotateView();
 		}
 		else{
 			print ("Please assign target first!");
